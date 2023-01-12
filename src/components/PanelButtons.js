bebe9879e64b960723f66dd70aceafbf85255301
Afeef <afeef9706@gmail.com>
2023-01-12 12:52:23 +0800

    Reordered the buttons

--- src/components/PanelButtons.js
+++ src/components/PanelButtons.js
@@ -1,16 +1,16 @@
 export default function PanelButtons() {
     return (
         <div id="panel-buttons" className="panel-buttons">
-            <div id="left-buttons" className="left-buttons">
-                <button id="one" className="button-pads">1</button>
-                <button id="two" className="button-pads">2</button>
-                <button id="three" className="button-pads">3</button>
+            <div id="left-buttons" className="left-buttons">            
+                <button id="seven" className="button-pads">7</button>
+                <button id="eight" className="button-pads">8</button>
+                <button id="nine" className="button-pads">9</button>    
                 <button id="four" className="button-pads">4</button>
                 <button id="five" className="button-pads">5</button>
                 <button id="six" className="button-pads">6</button>
-                <button id="seven" className="button-pads">7</button>
-                <button id="eight" className="button-pads">8</button>
-                <button id="nine" className="button-pads">9</button>
+                <button id="one" className="button-pads">1</button>
+                <button id="two" className="button-pads">2</button>
+                <button id="three" className="button-pads">3</button>               
                 <button id="zero" className="button-pads">0</button>
                 <button id="decimal" className="button-pads">.</button>
                 <button id="equals" className="button-pads">=</button>
