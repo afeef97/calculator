0ddf26f28d645c3e44ad73f3f9f1491a255b3870
Afeef <afeef9706@gmail.com>
2023-01-23 12:52:06 +0800

    Added number and decimal input function

--- src/components/CalculatorPanel.js
+++ src/components/CalculatorPanel.js
@@ -1,12 +1,33 @@
+import { useState } from "react";
 import PanelButtons from "./PanelButtons";
 import PanelScreen from "./PanelScreen";
 
 export default function Panel() {
+    // const [inputArray, setInputArray] = useState([]);
+    const [inputStr, setInputStr] = useState("0");
+
+    function handleClickNum(event) {
+        if(inputStr === "0"){
+            setInputStr("");
+        }
+        setInputStr(inputStr => inputStr.concat(event.target.innerText));
+    }
+
+    function handleClear() {
+        setInputStr("0");
+    }
+
+    function handleDecimal(event) {
+        if(!inputStr.includes(".")) {
+            setInputStr(inputStr => inputStr.concat(event.target.innerText));
+        }
+    }
+
     return (
         <div id="panel" className="panel">
             <div id="panel-content" className="panel-content">
-                <PanelScreen />
-                <PanelButtons />
+                <PanelScreen inputDisplay={inputStr}/>
+                <PanelButtons onClickNum={handleClickNum} onClear={handleClear} onClickDecimal={handleDecimal}/>
             </div>
         </div>
     );
