a8d7c566f2aa486be69b1506a7360bbd5ba92158
Afeef <afeef9706@gmail.com>
2023-01-27 18:46:52 +0800

    Added calculator functionality

--- src/components/CalculatorPanel.js
+++ src/components/CalculatorPanel.js
@@ -2,44 +2,99 @@ import { useState } from "react";
 import PanelButtons from "./PanelButtons";
 import PanelScreen from "./PanelScreen";
 
+const OPERATORS = {
+    '+': '+',
+    '-': '-',
+    '×': '*',
+    '÷': '/'
+}
+
 export default function Panel() {
     const [inputArray, setInputArray] = useState([]);
     const [inputStr, setInputStr] = useState("0");
-
+    const [result, setResult] = useState();
+    
     function handleClickNum(event) {
-        if(/^[0+×÷-]/.test(inputStr) && inputStr.length === 1){
+        if(result) {
+            setResult(0);
+        }
+
+        if(/^[0]{1}$/.test(inputStr) || inputStr === "Invalid Expression"){
+            setInputStr("");
+        }
+        else if(/^[+×÷-]{1}$/.test(inputStr)) {
+            setInputArray(prevArray => [...prevArray, OPERATORS[inputStr]]);
             setInputStr("");
         }
         setInputStr(prevStr => prevStr + event.target.innerText);
     }
 
-    function handleClickClear() {
-        setInputStr("0");
-        setInputArray([]);
+    function handleClickOperator(event) {
+        if(result) {
+            setInputStr(event.target.innerText);
+            setInputArray([...inputArray, result]);
+            setResult(0);
+        }
+        else if(!/^[+×÷-]{1}$/.test(inputStr)) {
+            setInputArray(prevArray => [...prevArray, inputStr]);
+        }
+        setInputStr(event.target.innerText);
+    }
+
+    function handleClickInvert() {
+        if(!/^[+×÷-]{1}$/.test(inputStr)) {
+            setInputStr(Number(inputStr * -1).toString());
+        }   
     }
 
     function handleClickDecimal(event) {
-        if(!inputStr.includes(".")) {
-            setInputStr(inputStr => inputStr.concat(event.target.innerText));
+        if(inputStr === "Invalid Expression") setInputStr("0.");
+        
+        else if(/^[+×÷-]{1}$/.test(inputStr) && !inputStr.includes(".")) {
+            setInputArray(prevArray => [...prevArray, OPERATORS[inputStr]]);
+            setInputStr("");
+            setInputStr(inputStr => inputStr + "0.");
+        }
+        else if(!inputStr.includes(".")) {
+            setInputStr(inputStr => inputStr + ".");
         }
     }
 
-    function handleClickOperator(event) {
-        if(!/^[0+×÷-]/.test(inputStr)) {
-            setInputArray(prevArray => [...prevArray, inputStr, event.target.innerText]);
-            setInputStr(event.target.innerText);
+    function handleClickEqual() {
+        let finalResult = 0;
+        let error = "";
+
+        try {
+            // eslint-disable-next-line
+            finalResult = eval(/[\d]/.test(inputStr) ? [...inputArray, inputStr].join(" ") : inputArray.join(" "));
         }
+        catch(err) {
+            error = "Invalid Expression";
+            setInputStr(error);
+        }
+        
+        setInputArray([]);
+        setInputStr("0");
+        setResult(finalResult);
     }
 
+    function handleClickClear() {
+        setInputStr("0");
+        setInputArray([]);
+        setResult(0);
+    }
+    
     return (
         <div id="panel" className="panel">
             <div id="panel-content" className="panel-content">
-                <PanelScreen inputDisplay={inputStr} outputDisplay={inputArray.join(" ")}/>
+                <PanelScreen inputDisplay={inputStr} outputDisplay={inputArray.join(" ")} res={result} />
                 <PanelButtons 
                     onClickNum={handleClickNum} 
                     onClickClear={handleClickClear}
                     onClickDecimal={handleClickDecimal} 
                     onClickOperator={handleClickOperator}
+                    onClickInvert={handleClickInvert}
+                    onClickEqual={handleClickEqual}
                 />
             </div>
         </div>
--- src/components/PanelScreen.js
+++ src/components/PanelScreen.js
@@ -1,7 +1,7 @@
 export default function PanelScreen(props) {
     return (
         <div id="display" className="display">
-            <div className="output-display">{props.outputDisplay}</div>
+            <div className="output-display">{props.res ? props.res : props.outputDisplay}</div>
             <div className="user-input-display">{props.inputDisplay}</div>
         </div>
     );
